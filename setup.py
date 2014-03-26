401ef1535eb8e260233c71f7c252d318ad7e3dff
Rob Horner <rohorner@cisco.com>
2014-03-26 11:50:39 -0600

    updated with author metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
+
 setup(name='pycimc',
       version='0.1',
+      description='Python interface to Cisco UCS rack-mount server CIMC XML API',
+      author='Rob Horner',
+      author_email='robert@horners.org',
       py_modules=['pycimc', 'pycimcexpect'],
       )
