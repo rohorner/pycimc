ba83dc5315852ef217296d235c159ab940b1d529
Rob Horner <rohorner@cisco.com>
2014-03-26 12:01:37 -0600

    added requirement for 'requests' package

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pycimc',
       version='0.1',
       description='Python interface to Cisco UCS rack-mount server CIMC XML API',
       author='Rob Horner',
       author_email='robert@horners.org',
-      py_modules=['pycimc', 'pycimcexpect'],
+      py_modules=['pycimc',
+                  'pycimcexpect'],
+      install_requires=[
+          "requests >= 2.2.1",
+          ],
       )
