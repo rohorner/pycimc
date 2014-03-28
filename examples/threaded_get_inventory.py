8ba587c7acde392a6620eca912564bcf3a0d75dc
Rob Horner <rohorner@cisco.com>
2014-03-28 16:28:07 -0600

    misc bug fixes

--- examples/threaded_get_inventory.py
+++ examples/threaded_get_inventory.py
@@ -1,6 +1,6 @@
 __author__ = 'Rob Horner (robert@horners.org)'
 
-from pycimc import *
+from pycimc import UcsServer
 import config
 from Queue import Queue
 from threading import Thread, Lock
@@ -24,25 +24,34 @@ class ThreadLogin(Thread):
             server = UcsServer(host, config.USERNAME, config.PASSWORD)
             try:
                 if server.login():
-                    if server.get_fw_versions():
-                        out_string = host + '\t'
-                        for key,value in server.inventory['fw'].items():
-                            # split up the DN (sys/rack-unit-1/adaptor-1/mgmt/fw) key and remove the common
-                            # 'sys/rack-unit-1' prefix to reduce text size
-                            key = '/'.join(key.split('/')[2:])
-                            out_string += key + '\t' + value + '\t'
-                        # grab a lock for writing to sysout
-                        with self.lock:
-                            print out_string
+                    #if server.get_fw_versions():
+                    #    out_string = host
+                    #    for key in sorted(server.inventory['fw'], reverse=True):
+                    #        # split up the DN (sys/rack-unit-1/adaptor-1/mgmt/fw) key and remove the common
+                    #        # 'sys/rack-unit-1' prefix to truncate the DN
+                    #        truncated_dn = '/'.join(key.split('/')[2:])
+                    #        out_string += ',' + truncated_dn + ',' + server.inventory['fw'][key]
+                    #    # grab a lock for writing to sysout
+                    #    with self.lock:
+                    #        print out_string
+                    #if server.get_boot_order():
+                    #    with self.lock:
+                    #        print "{0}\t{1}".format(host, server.inventory['boot_order'])
                     #if server.get_chassis_info():
                     #    with self.lock:
                     #        #print host,":",server.chassis_info
                     #        print "{0}: {1}".format(host, server.model)
+                    if server.get_eth_settings():
+                        with self.lock:
+                            print host
+                            print server.inventory['pci']
+                            print server.inventory['adaptor']
+                            print '\n'
 
                     server.logout()
-                else:
-                    with self.lock:
-                        print "Couldn't log in to", host
+                #else:
+                #    with self.lock:
+                #        print "Couldn't log in to", host
             except Exception as err:
                 with self.lock():
                     print "Server Error:", host, err
