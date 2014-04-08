4d45cc7a9ca20942fdc46663702ea6e88a4a7af0
Rob Horner <rohorner@cisco.com>
2014-04-08 08:57:38 -0600

    print out VIC interface info in CSV format

--- examples/get_inventory.py
+++ examples/get_inventory.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 from pycimc import *
 import config
+from pprint import pprint
 
 __author__ = 'Rob Horner (robert@horners.org)'
 
@@ -7,7 +10,15 @@ for address in config.SERVERS:
     server = UcsServer(address, config.USERNAME, config.PASSWORD)
     if server.login():
         print 'server:', address
-        server.get_fw_versions()
+        server.get_interface_inventory()
+        # pprint(server.inventory['adaptor'])
+        for int in server.inventory['adaptor']:
+            out_string = 'SLOT-'+int['pciSlot']
+            for port in int['port']:
+                out_string += ',port-'+str(port['portId'])+','+port['adminSpeed']+','+port['linkState']
+                for vnic in port['vnic']:
+                    out_string += ','+str(vnic['name'])+','+str(vnic['mac'])
+            print out_string
         server.logout()
     else:
         continue
