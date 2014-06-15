a8bd5b36499f67e9be9dc360d143165a645665fa
Rob Horner <robert@horners.org>
2014-06-14 20:39:34 -0600

    updated for version 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pycimc',
-      version='0.5.2',
+      version='0.6.0',
       description='Python interface to Cisco UCS rack-mount server CIMC XML API',
       author='Rob Horner',
       author_email='robert@horners.org',
