33b31358d98002515f37aab3778e77200b821bc5
Rob Horner <robert@horners.org>
2016-10-13 09:36:19 -0600

    changed to use config.py login parameters

--- examples/get_inventory.py
+++ examples/get_inventory.py
@@ -4,20 +4,17 @@ from pycimc import *
 import config
 from pprint import pprint
 
-ADDRESS = '192.168.200.101'
-USERNAME = 'admin'
-PASSWORD = 'password'
 
-
-with UcsServer(ADDRESS, USERNAME, PASSWORD) as server:
-    out_string = server.ipaddress
-    if server.get_interface_inventory():
-        for int in server.inventory['adaptor']:
-            out_string += ',SLOT-'+int['pciSlot']
-            for port in int['port']:
-                out_string += ',port-'+str(port['portId'])+','+port['adminSpeed']+','+port['linkState']
-                for vnic in port['vnic']:
-                    out_string += ','+str(vnic['name'])+','+str(vnic['mac'])
-            print out_string
-    else:
-        print 'get_interface_inventory() returned False'
+for address in config.SERVERS:
+    with UcsServer(address, config.USERNAME, config.PASSWORD) as server:
+        out_string = server.ipaddress
+        if server.get_interface_inventory():
+            for int in server.inventory['adaptor']:
+                out_string += ',SLOT-'+int['pciSlot']
+                for port in int['port']:
+                    out_string += ',port-'+str(port['portId'])+','+port['adminSpeed']+','+port['linkState']
+                    for vnic in port['vnic']:
+                        out_string += ','+str(vnic['name'])+','+str(vnic['mac'])
+                print out_string
+        else:
+            print 'get_interface_inventory() returned False'
